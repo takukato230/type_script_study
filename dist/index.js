bfcc6b706e16b8884ada0f06a1dc19fe3d98ee89
takumi katori <>
2020-10-19 16:39:09 +0900

    settiong typescript

---
+++ dist/index.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let a = 1 + 2;
+let b = a + 3;
+let c = {
+    apple: a,
+    banana: b
+};
+let d = c.apple * 4;
+console.log(d);
