fb0a7c84541a838ec49bdeecd013128482eab29e
takumi katori <>
2020-11-07 20:24:28 +0900

    add code

--- src/chapter04/function_type.ts
+++ src/chapter04/function_type.ts
@@ -1,4 +1,3 @@
-import { ResolvedTypeReferenceDirectiveWithFailedLookupLocations } from "typescript"
 
 // 呼び出しシグニチャの省略記法
 type LogT = (message: string, userID?: string) => void
@@ -12,3 +11,33 @@ type Reserve = {
   (from: Date, to: Date, destination: string): string
   (from: Date, destination: string): string
 }
+
+// reserve  
+// Reserveが実装される際にはそれらがどのように呼び出されたかチェックする必要がある
+// let reserve: Reserve = (
+//   from: Date,
+//   toOrDestination: Date | string,
+//   destination?: string
+// ) => {
+//   if (toOrDestination instanceof Date && destination !== undefined) {
+//     // 宿泊旅行を予約する
+//   } else if (typeof toOrDestination === 'string') {
+//     // 日帰り旅行を予約する
+//   }
+// }
+
+type WarnUser = {
+  (warning: string): void
+  wasCalled: boolean
+}
+
+// function warnUser(waring: string) {
+//   if (warnUser.wasCalled) {
+//     return 
+//   }
+//   warnUser.wasCalled = true
+//   alert(warning)
+
+// }
+
+// const assignedWarnUser: WarnUser = warnUser
