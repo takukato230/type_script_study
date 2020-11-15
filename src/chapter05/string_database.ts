c0a294788864376a10b4c9408b7c25b28c224bcb
takumi katori <>
2020-11-15 22:58:40 +0900

    add ts file

---
+++ src/chapter05/string_database.ts
@@ -0,0 +1,20 @@
+type DBState = {
+  [key: string]: string
+}
+
+class StringDatabase {
+  state: DBState = {}
+  get(key: string): string | null {
+    return key in this.state ? this.state[key] : null
+  }
+  set(key: string, value: string): void {
+    this.state[key] = value
+  }
+  static from(state: DBState) {
+    let db = new StringDatabase
+    for (const k in state) {
+      db.set(k, state[k])
+    }
+    return db
+  }
+}
